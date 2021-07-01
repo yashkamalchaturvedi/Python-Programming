ee292b0741ff299f9b0a16e32ab7c2f897310632
Yash Kamal Chaturvedi <69413160+yashkamalchaturvedi@users.noreply.github.com>
2021-07-01 22:54:24 +0530

    Delete Chapter 7 directory

--- Chapter 7/pr01.py
+++
@@ -1,4 +0,0 @@
-num = int(input("enter the number"))
-for i in range(1, 11):
-    # print(str(num) + "X" + str(i) +"=" + str(i*num))
-    print(f"{num}X{i}={num*i}")
--- Chapter 7/tut2.py
+++
@@ -1,4 +0,0 @@
-fs = ['B', 'W', 'C', 'E']
-
-for i in fs:
-    print(i)
