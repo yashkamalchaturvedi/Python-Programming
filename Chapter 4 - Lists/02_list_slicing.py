8496b60a27d1694d11b5bfcae438d4dabcc8f849
Yash Kamal Chaturvedi <69413160+yashkamalchaturvedi@users.noreply.github.com>
2021-07-02 08:03:21 +0530

    Delete Chapter 4 - Lists directory

--- Chapter 4 - Lists/02_list_slicing.py
+++
@@ -1,4 +0,0 @@
-# List slicing
-friends = ["harry", "tom", "rohan", "sam", "divya", 45]
-print(friends[0:4])
-print(friends[-4:])
--- Chapter 4 - Lists/05_tuple_methods.py
+++
@@ -1,3 +0,0 @@
-t = (1,2,4,5,1,1,1)
-# print(t.count(1))
-print(t.index(1))
